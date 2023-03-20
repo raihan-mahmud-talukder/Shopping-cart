c7e4d11dc16d8371c080e0e47059ad68841aa6ed
Raihan Mahmud <raihan_uits@outlook.com>
2023-03-20 06:45:47 +0600

    basket total

--- index.js
+++ index.js
@@ -2,52 +2,69 @@ const products = [
     {
         name: 'Nature 1',
         img: 'nature1',
-        price: 100,
-        inCart: 0
+        price: 103,
+        inCart: 0,
+        total: 0
     },
     {
         name: 'Nature 2',
         img: 'nature2',
-        price: 100,
-        inCart: 0
+        price: 105,
+        inCart: 0,
+        total: 0
     },
     {
         name: 'Nature 3',
         img: 'nature3',
-        price: 100,
-        inCart: 0
+        price: 101,
+        inCart: 0,
+        total: 0
     },
     {
         name: 'Nature 4',
         img: 'nature4',
-        price: 100,
-        inCart: 0
+        price: 99,
+        inCart: 0,
+        total: 0
     },
     {
         name: 'Nature 5',
         img: 'nature5',
-        price: 100,
-        inCart: 0
+        price: 97,
+        inCart: 0,
+        total: 0
     },
     {
         name: 'Nature 6',
         img: 'nature6',
-        price: 100,
-        inCart: 0
+        price: 95,
+        inCart: 0,
+        total: 0
     },
     {
         name: 'Nature 7',
         img: 'nature7',
         price: 100,
-        inCart: 0
+        inCart: 0,
+        total: 0
     },
     {
         name: 'Nature 8',
         img: 'nature8',
-        price: 100,
-        inCart: 0
+        price: 110,
+        inCart: 0,
+        total: 0
     }
 ]
+
+document.getElementsByTagName('main')[0].getElementsByTagName('div')[0].getElementsByTagName('h5')[0].innerHTML = `BDT ${products[0].price}`
+document.getElementsByTagName('main')[0].getElementsByTagName('div')[1].getElementsByTagName('h5')[0].innerHTML = `BDT ${products[1].price}`
+document.getElementsByTagName('main')[0].getElementsByTagName('div')[2].getElementsByTagName('h5')[0].innerHTML = `BDT ${products[2].price}`
+document.getElementsByTagName('main')[0].getElementsByTagName('div')[3].getElementsByTagName('h5')[0].innerHTML = `BDT ${products[3].price}`
+document.getElementsByTagName('main')[0].getElementsByTagName('div')[4].getElementsByTagName('h5')[0].innerHTML = `BDT ${products[4].price}`
+document.getElementsByTagName('main')[0].getElementsByTagName('div')[5].getElementsByTagName('h5')[0].innerHTML = `BDT ${products[5].price}`
+document.getElementsByTagName('main')[0].getElementsByTagName('div')[6].getElementsByTagName('h5')[0].innerHTML = `BDT ${products[6].price}`
+document.getElementsByTagName('main')[0].getElementsByTagName('div')[7].getElementsByTagName('h5')[0].innerHTML = `BDT ${products[7].price}`
 
 let cart = 0
 const cartNumber = document.getElementsByTagName('li')[0].getElementsByTagName('span')[0]
@@ -63,19 +80,17 @@ for (let i = 0; i < carts.length; i++) {
             addToCart(products[i])
             cart++
         } else {
-            // products[i].inCart++
             console.log(products[i].inCart);
             alert('Product has been Already added to the cart!')
         }
-        // console.log(products[i], cart)
         cartNumber.innerHTML = `(${cart})`
     })
 }
 
-const productsCart = document.getElementById('products')
-const basketTotal = document.getElementById('basket')
 
 const addToCart = product => {
+    const productsCart = document.getElementById('products')
+    const basket = document.getElementById('basket').getElementsByTagName('h4')[1]
     productsCart.innerHTML +=
         `<div id='products'>
             <div class='products'>
@@ -90,24 +105,24 @@ const addToCart = product => {
                 <span class='increase'> >> </span>
             </div>
             <span class='total'>${product.price * product.inCart}</span>
-            </div>
-            `
-    basketTotal.innerHTML +=
-    `
-    <div class='basket'>
-    <h4>Basket Total</h4>
-    <h4>Total Cost ${product.price}</h4>
-    </div>
-    `
+        </div>
+        `
 
+    const table = document.getElementById('products').querySelectorAll('#products')
     const productList = document.querySelectorAll('.remove')
     const decreaseProduct = document.querySelectorAll('.decrease')
     const increaseProduct = document.querySelectorAll('.increase')
 
+    let basketTotal = 0
+
     for (let i = 0; i < productList.length; i++) {
+        basketTotal += cartProduct[i].inCart * cartProduct[i].price
+        basket.innerHTML = basketTotal
         productList[i].addEventListener('click', event => {
             event.target.parentElement.parentElement.remove()
             cart -= cartProduct[i].inCart
+            basketTotal -= cartProduct[i].total
+            basket.innerHTML = basketTotal
             cartNumber.innerHTML = `(${cart})`
         })
         decreaseProduct[i].addEventListener('click', event => {
@@ -116,19 +131,24 @@ const addToCart = product => {
             } else {
                 cart--
                 cartProduct[i].inCart--
+                cartProduct[i].total = cartProduct[i].inCart * cartProduct[i].price
+                basketTotal -= cartProduct[i].price
+                basket.innerHTML = basketTotal
                 cartNumber.innerHTML = `(${cart})`
                 event.target.nextElementSibling.innerHTML = cartProduct[i].inCart
                 event.target.parentElement.nextElementSibling.innerHTML = cartProduct[i].inCart * cartProduct[i].price
-                console.log(cartProduct[i]);
             }
         })
         increaseProduct[i].addEventListener('click', event => {
             cart++
             cartProduct[i].inCart++
+            cartProduct[i].total = cartProduct[i].inCart * cartProduct[i].price
+            basketTotal += cartProduct[i].price
+            basket.innerHTML = basketTotal
             cartNumber.innerHTML = `(${cart})`
             event.target.previousElementSibling.innerHTML = cartProduct[i].inCart
             event.target.parentElement.nextElementSibling.innerHTML = cartProduct[i].inCart * cartProduct[i].price
-            console.log(cartProduct[i]);
         })
+        // if (i % 2 == 1) {table[i].style.background = 'lightcyan'}
     }
 }
